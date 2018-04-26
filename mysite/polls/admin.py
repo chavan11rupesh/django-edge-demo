f0684eeeb66d82c624aa5b42cc2402f48e97b3e7
chavan11 <chavan04rupesh@gmail.com>
2018-04-26 16:27:52 +0530

    made some changes & added rangefilter

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
-from .models import Question, Choice, QuestionAdmin
+from .models import Question, Choice
+from rangefilter.filter import DateRangeFilter
+from django.utils.html import format_html
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # to not show selected counter
+    def delete_button(self, obj):
+        return format_html('<a class="btn" href="/admin/my_app/my_model/{}/delete/">Delete</a>', obj.id)
+    actions_selection_counter = False
+    # to show dates
+    date_hierarchy = 'date_created'
+    # to add search bar
+    search_fields = ['set_no','level_no']
+    # to show only required fields on admin panel
+    list_display = ('practice_question','answer','set_no','level_no','delete_button')
+    # to add filter on the right side
+    list_filter = ('practice_question','set_no',('date_created',DateRangeFilter))
+    # to add sections while adding data
+    fieldsets = (
+        ('Section1',{
+            'fields' : ('practice_question','answer')
+        }),
+        ('Section2',{
+            'fields' : ('hint1','hint2','hint3')
+        }),
+        ('Section3',{
+            'fields' : (('set_no','level_no'),'date_created')
+        }),)
+    # to show empty value
+    empty_value_display = '-empty-'
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,14 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
-from django.contrib.admin import DateFieldListFilter
-
-
-class CSSAdminMixin(object):
-    class Media:
-        css = {
-            'all' : ('css/admin.css',),
-        }
 
 
 class Question(models.Model):
@@ -21,28 +13,6 @@ class Question(models.Model):
     set_no = models.IntegerField(default=0)
     level_no = models.IntegerField(default=0)
     date_created = models.DateTimeField()
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    #to add search bar
-    search_fields = ['set_no','level_no']
-    # to show only required fields on admin panel
-    list_display = ('practice_question','answer','set_no','level_no')
-    # to add filter on the right side
-    list_filter = ('practice_question','set_no',('date_created',DateFieldListFilter))
-    # to add sections while adding data
-    fieldsets = (
-        ('Section1',{
-            'fields' : ('practice_question','answer')
-        }),
-        ('Section2',{
-            'fields' : ('hint1','hint2','hint3')
-        }),
-        ('Section3',{
-            'fields' : ('set_no','level_no','date_created')
-        })
-    )
-
 
 
 class Choice(models.Model):
